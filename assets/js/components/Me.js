98004225bd61e0e6af48a34aab0cc09bca99e454
Ju <julien.castera@ynov.com>
2020-10-01 22:42:41 +0200

    :construction: Base version

---
+++ assets/js/components/Me.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Me extends Component {
+    render() {
+        return (
+            <div>
+                Me
+            </div>
+        );
+    }
+}
+
+
+export default Me;
